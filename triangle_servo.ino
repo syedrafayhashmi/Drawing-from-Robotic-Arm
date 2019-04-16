a4c5aa03291ac56902ba6ab5cbfcc3bb08052c90
Syed Rafay Hashmi <41583760+syedrafayhashmi@users.noreply.github.com>
2019-04-16 18:34:28 +0500

    Add files via upload

---
+++ triangle_servo.ino
@@ -0,0 +1,24 @@
+
+#include <Servo.h>
+
+Servo myservo;  
+
+int pos = 0;   
+
+void setup() {
+  myservo.attach(9);  
+}
+
+void loop() {
+  for (pos = 45; pos <= 90; pos +=  6) { 
+    myservo.write(pos);   
+    delay(20);            
+  }
+  for (pos = 90; pos >= 45; pos -= 5
+  
+  ) { 
+    myservo.write(pos); 
+    delay(20);           
+  }
+}
+
